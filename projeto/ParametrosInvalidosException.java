87bf537891e1f09669f4615f71d40fb67ea645f9
Daniel Sarcinelli Frasson <130710428+danielsfrasson@users.noreply.github.com>
2024-07-31 16:23:14 -0300

    Update ParametrosInvalidosException.java

--- projeto/ParametrosInvalidosException.java
+++ projeto/ParametrosInvalidosException.java
@@ -2,6 +2,6 @@ package projeto;
 
 public class ParametrosInvalidosException extends Exception {
     public ParametrosInvalidosException(){
-
+        super("O segundo parâmetro deve ser maior que o primeiro.");
     }
 }
